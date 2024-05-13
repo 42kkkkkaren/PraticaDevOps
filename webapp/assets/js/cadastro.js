09a337bf6620dbab90af526a2a23766b38b9a9f0
Matheus <mthsbessac@gmail.com>
2024-05-13 11:22:04 -0300

    Alterações nas páginas

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -4,35 +4,19 @@ function criarUsuario(event) {
     event.preventDefault();
     console.log("Handler for form submit invoked.");
 
-    var senha = $('#senha').val();
-    var confirmarSenha = $('#confirmar-senha').val();
-
-    if (senha !== confirmarSenha) {
+    if ($('#senha').val() != $('#confirmar-senha').val()) {
         alert("As senhas não coincidem");
         return;
     }
 
-    var userData = {
-        nome: $('#nome').val(),
-        email: $('#email').val(),
-        nick: $('#nick').val(),
-        senha: senha
-    };
-
-    console.log("Prepared user data:", userData);
-
-    $.ajax({
-        url: "/http:/localhost:8080/usuarios",
+    $.ajax ({
+        url: "/usuarios",
         method: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(userData),
-        success: function(response) {
-            console.log('User registration successful:', response);
-            alert('User registered successfully!');
+        data: {
+            nome: $('#nome').val(),
+            email: $('#email').val(),
+            nick: $('#nick').val(),
+            senha: $('#senha').val()
         },
-        error: function(xhr, status, error) {
-            console.error('Registration failed:', status, error);
-            alert('Failed to register user.');
-        }
     });
 }
--- webapp/main.go
+++ webapp/main.go
@@ -1,47 +1,16 @@
-/*
 package main
 
 import (
-	"log"
 	"net/http"
-	"webapp/src/router"
-	"webapp/src/utils"
 )
 
 func main() {
+	// Serve static assets like CSS and JavaScript
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	utils.CarregarTemplates()
-	r := router.Gerar()
-	// Corrected route for serving static files
-	fileServer := http.FileServer(http.Dir("./assets/"))
-	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+	// Serve HTML files
+	http.Handle("/", http.FileServer(http.Dir("views")))
 
-	// Make sure the static file handler is registered within the router
-	log.Println("Serving files on http://localhost:8080/assets/")
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
-*/
-
-package main
-
-import (
-    "log"
-    "net/http"
-)
-
-func main() {
-    // Serve static files from the "webapp" directory
-    fs := http.FileServer(http.Dir("./views/"))
-    http.Handle("/", fs)
-
-    log.Println("Server running on http://localhost:8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
-
