537c17d9c87f780232f7e8e77d0bf4e2276ff427
Matheus <mthsbessac@gmail.com>
2024-06-11 12:13:53 -0300

    Adding Bootstrap

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/modelos"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -16,5 +21,20 @@ func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 
 // CarregarPaginaPrincipal renderiza a página principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
-	utils.ExecutarTemplate(w, "home.html", nil)
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
