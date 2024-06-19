4a91f5e460576f86f6b81527e0ffc5f105128ecf
Matheus <mthsbessac@gmail.com>
2024-06-19 09:49:35 -0300

    Commit

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -1,27 +1,39 @@
 $('#formulario-cadastro').on('submit', criarUsuario);
 
-function criarUsuario(event) {
-    event.preventDefault();
-    console.log("Handler for form submit invoked.");
+function criarUsuario(evento) {
+    evento.preventDefault();
 
     if ($('#senha').val() != $('#confirmar-senha').val()) {
-        alert("As senhas não coincidem");
+        Swal.fire("Ops...", "As senhas não coincidem!", "error");
         return;
     }
 
-    $.ajax ({
+    $.ajax({
         url: "/usuarios",
         method: "POST",
         data: {
-            nome: $('#nome').val(),
-            email: $('#email').val(),
-            nick: $('#nick').val(),
-            senha: $('#senha').val(),
-        },
+           nome: $('#nome').val(), 
+           email: $('#email').val(),
+           nick: $('#nick').val(),
+           senha: $('#senha').val()
+        }
     }).done(function() {
-        alert("Usuário cadastrado com sucesso!");
-        window.location.href = "/login";
+        Swal.fire("Sucesso!", "Usuário cadastrado com sucesso!", "success")
+            .then(function() {
+                $.ajax({
+                    url: "/login",
+                    method: "POST",
+                    data: {
+                        email: $('#email').val(),
+                        senha: $('#senha').val()
+                    }
+                }).done(function() {
+                    window.location = "/home";
+                }).fail(function() {
+                    Swal.fire("Ops...", "Erro ao autenticar o usuário!", "error");
+                })
+            })
     }).fail(function() {
-        alert("Erro ao cadastrar usuário");
+        Swal.fire("Ops...", "Erro ao cadastrar o usuário!", "error");
     });
 }
--- webapp/src/respostas/respostas.go
+++ webapp/src/respostas/respostas.go
@@ -2,27 +2,28 @@ package respostas
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
-// JSON retorna uma resposta em formato JSON
-func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	w.Write(dados.([]byte))
-
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		http.Error(w, "Houve um erro interno no servidor", http.StatusInternalServerError)
-		return
-	}
-}
-
-// Erro retorna um erro em formato JSON
+// ErroAPI representa a resposta de erro da API
 type ErroAPI struct {
 	Erro string `json:"erro"`
 }
 
-// TratarStatusCodeDeErro trata o status code de erro 400 ou superior
+// JSON retorna uma resposta em formato JSON para a requisição
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if statusCode != http.StatusNoContent {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
+	}
+}
+
+// TratarStatusCodeDeErro trata as requisições com status code 400 ou superior
 func TratarStatusCodeDeErro(w http.ResponseWriter, r *http.Response) {
 	var erro ErroAPI
 	json.NewDecoder(r.Body).Decode(&erro)
