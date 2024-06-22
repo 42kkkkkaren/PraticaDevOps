7f85f23b203a91e9e86a6a344b6e36169050ee27
Matheus <mthsbessac@gmail.com>
2024-06-22 12:39:11 -0300

    resolvendo conflito CORS

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"api/src/config"
+	"api/src/middlewares"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -11,6 +12,8 @@ import (
 func main() {
 	config.CarregarEnv()
 	r := router.Gerar()
+
+	r.Use(middlewares.EnableCors)
 
 	fmt.Printf("Escutando na porta %d\n", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
--- api/src/router/router.go
+++ api/src/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"api/src/router/rotas"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -9,6 +10,14 @@ import (
 // Gerar vai retornar um Router com as rotas configuradas
 func Gerar() *mux.Router {
 	r := mux.NewRouter()
+
+	// Adiciona uma rota OPTIONS para todas as rotas
+	r.Methods(http.MethodOptions).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.WriteHeader(http.StatusOK)
+	})
 
 	return rotas.ConfigurarRotas(r)
 }
